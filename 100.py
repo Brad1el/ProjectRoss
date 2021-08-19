3c6bf511a372d248c1eb416e41922f13e01d8b61
Brad1el <89223272+Brad1el@users.noreply.github.com>
2021-08-19 15:34:49 -0500

    Add files via upload

---
+++ 100.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 17 16:49:36 2021
+
+@author: thegr
+"""
+
+play = False
+    
+def Playing():
+    while play = True:
+        
+def GetName():
+    return str(input())
+    
+
+
+def main():
+    nam = ""
+    print("Enter your name", end = " ")
+    nam = GetName()
+    print("So your name is " + nam + "?")
+    
+       
+main()
